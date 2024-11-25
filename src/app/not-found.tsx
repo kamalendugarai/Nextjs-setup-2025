19305aba36a20eaffa1b16a5f24068eafba5e1c5
Kamalendu Garai <kamalendu.garai@massoftind.com>
2024-11-25 18:13:47 +0530

    Fixed that from Modified font of 404 and others

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,12 +7,12 @@ import {
 	CardHeader,
 	CardTitle
 } from '@/components/ui/card';
-import { Alegreya } from 'next/font/google';
+import { Anton } from 'next/font/google';
 
-const alegreya = Alegreya({
+const anton = Anton({
 	subsets: ['latin'],
 	display: 'swap',
-	weight: '600'
+	weight: '400'
 });
 
 const NotFound = () => {
@@ -21,7 +21,10 @@ const NotFound = () => {
 			<Card className='max-w-[600px] flex-1 items-center text-center rounded-xl mb-2'>
 				<CardHeader>
 					<CardTitle>
-						<h1 className={`text-[30rem] color ${alegreya.className}`}>404</h1>
+						<h1
+							className={`text-[30rem] text-gray-700 drop-shadow-sm ${anton.className}`}>
+							404
+						</h1>
 					</CardTitle>
 					<CardDescription className='text-2xl'>
 						The content you are looking for does not exist.
