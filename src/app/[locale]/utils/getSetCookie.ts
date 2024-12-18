614b59f2f888f7bf1cc255262ee79053848ec898
Kamalendu Garai <kamalendu.garai@massoftind.com>
2024-12-18 17:35:54 +0530

    Added show log to getSetCookie

--- src/app/[locale]/utils/getSetCookie.ts
+++ src/app/[locale]/utils/getSetCookie.ts
@@ -6,7 +6,9 @@ import { getByteSize } from './getByteSize';
 const doEncrypt = async (elem: string, recog?: string, salt?: string) => {
 	if (!!process.env.ENCRYPTSTORAGE?.trim()) {
 		if (recog?.toLowerCase() === 'key') {
-			return CryptoJS.SHA256(elem).toString();
+			const enCryptedValue = CryptoJS.SHA256(elem).toString();
+			show.log(enCryptedValue);
+			return enCryptedValue;
 		}
 		const salty = salt || process.env.NEXT_ENCRYPTION_SALT;
 		const encrypted = CryptoJS.AES.encrypt(elem, salty as string).toString();
@@ -19,15 +21,22 @@ const doEncrypt = async (elem: string, recog?: string, salt?: string) => {
 const doDecrypt = async (elem: string, salt?: string) => {
 	if (!!process.env.ENCRYPTSTORAGE?.trim()) {
 		const salty = salt || process.env.NEXT_ENCRYPTION_SALT;
-		return CryptoJS.AES.decrypt(elem, salty as string).toString(
+		const decryptedValue = CryptoJS.AES.decrypt(elem, salty as string).toString(
 			CryptoJS.enc.Utf8
 		);
+		show.log(decryptedValue);
+		return decryptedValue;
 	}
+
 	return elem;
 };
 
 const ownCookies = {
-	cook: async (name: string, value: unknown, options: any) => {
+	cook: async (
+		name: string,
+		value: unknown,
+		options: Record<string, unknown>
+	) => {
 		const cookieSize = getByteSize(value as string);
 		if (cookieSize < 4) {
 			const cookieStore = await cookies();
@@ -35,9 +44,11 @@ const ownCookies = {
 			const cookieValue = await doEncrypt(value as string);
 			cookieStore.set(cookieName, cookieValue, {
 				httpOnly: true,
-				strict: process.env.NODE_ENV === 'production',
+				sameSite: 'strict',
+				secure: process.env.NODE_ENV === 'production',
 				...options
 			});
+			show.log(cookieName, cookieValue);
 			return cookieStore.get(cookieName);
 		} else {
 			console.log('Cookie size is too big to store in cookies');
@@ -49,6 +60,7 @@ const ownCookies = {
 		const cookieValue = await doDecrypt(
 			cookieStore.get(cookieName) as unknown as string
 		);
+		show.log(cookieName, cookieValue);
 		return cookieValue;
 	}
 };
